5bc30f568d92e9ccbf692c83df031ed9c419787c
zhang <zhang.heling@foxmail.com>
2025-01-26 21:20:06 +0800

    neovide

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local g = vim.g
 opt.showmatch = true
 opt.autoindent = true
 opt.smartindent = true
@@ -11,15 +12,16 @@ opt.relativenumber = false
 opt.jumpoptions = "stack"
 -- opt.winblend = 0
 opt.list = false
--- opt.cursorline = false
--- vim.g.lazyvim_python_lsp = "basedpyright"
--- vim.g.lazyvim_python_ruff = "ruff_lsp"
-if vim.g.neovide then
-  vim.g.neovide_cursor_animation_length = 0
+g.trouble_lualine = false
+opt.showcmd = false -- 禁用按键显示
+g.snacks_animate = false
+if g.neovide then
+  g.neovide_cursor_animation_length = 0
   vim.o.guifont = "JetBrainsMono Nerd Font:h11"
   vim.opt.linespace = -3
-  vim.g.neovide_theme = "dark"
-  vim.g.neovide_title_text_color = "#1f1f1f"
+  g.neovide_theme = "dark"
+  vim.g.neovide_title_background_color =
+    string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
+  -- vim.g.neovide_transparency = 0.95
+  -- vim.g.neovide_normal_opacity = 0.95
 end
-
--- vim.g.lazyvim_pickey = "telescope"
